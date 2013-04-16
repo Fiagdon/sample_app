cbff6210440149ed468e74843ca75c8abd4065b0
Maxim Sinitsyn <maxim.sinitsyn@gmail.com>
2013-04-16 02:26:57 -0400

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -10,12 +10,13 @@ gem 'pg'
 gem 'taps'
 
 group :development do
- gem 'rspec-rails', '2.0.1'
+ gem 'rspec-rails', '2.4.0'
 end
 
 group :test do
- gem 'rspec', '2.0.1'
+ gem 'rspec', '2.4.0'
  gem 'webrat', '0.7.1'
+ gem 'spork', '0.8.4'
 end
 
 # Gems used only for assets and not required
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
+require 'rails/all'
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
@@ -64,5 +65,15 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    ### Part of a Spork hack. See http://bit.ly/arY19y
+    if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+        # Work around initializer in railties/lib/rails/application/bootstrap.rb
+        ActiveSupport::Dependencies.mechanism = :load
+      end
+    end
+      
+    
   end
 end
